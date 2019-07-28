3f7e0a8776985a96760643efefcd4c1291e167f8
wizordgamming <wizordgamming@gmail.com>
2019-07-28 13:20:51 +1200

    adding frameworks and test pages

---
+++ cesiumtest.js
@@ -0,0 +1,42 @@
+
+
+// NOTE: Viewer constructed after default view is set.
+//loading and making map
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ODQyYjRlNS05NDg1LTQyM2YtOTJhOS0wODljNjM0MDIxMzIiLCJpZCI6MTM4NTcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjQxNzgyNDB9.kVn00wi5JwJRS2XyAtYJX12x-jA4EapEscOzw2De16I';
+  var viewer = new Cesium.Viewer('cesiumContainer', {
+    // imageryProvider : Cesium.createTileMapServiceImageryProvider({
+    //   url : Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+    // }),
+    terrainProvider : new Cesium.CesiumTerrainProvider({
+        url: Cesium.IonResource.fromAssetId(1)
+    }),
+    baseLayerPicker : false,
+    geocoder : false,
+    timeline	:false,
+    animation:false
+  });
+
+
+      var west = 176.0;
+      var south = -40.8;
+      var east = -185;
+      var north = -40.275;
+
+      var rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
+      viewer.camera.setView({
+          destination: rectangle
+      });
+
+      var pinBuilder = new Cesium.PinBuilder();
+
+      var hospitalPin = Cesium.when(pinBuilder.fromMakiIconId('hospital', Cesium.Color.RED, 48), function(canvas) {
+    return viewer.entities.add({
+        name : 'Hospital',
+        position : Cesium.Cartesian3.fromDegrees(175.6223059,-40.3876416,15.45),
+        description:"memes for dreams<h1> big memes</h1>",
+        billboard : {
+            image : canvas.toDataURL(),
+            verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+        }
+    });
+});
