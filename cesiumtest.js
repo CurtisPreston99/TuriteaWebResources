e3bef3ccf339301d72d2695df18106a92a63aad8
wizordgamming <wizordgamming@gmail.com>
2019-08-09 20:20:01 +1200

    adding pin loading

--- cesiumtest.js
+++ cesiumtest.js
@@ -1,5 +1,6 @@
 
 
+
 // NOTE: Viewer constructed after default view is set.
 //loading and making map
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ODQyYjRlNS05NDg1LTQyM2YtOTJhOS0wODljNjM0MDIxMzIiLCJpZCI6MTM4NTcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjQxNzgyNDB9.kVn00wi5JwJRS2XyAtYJX12x-jA4EapEscOzw2De16I';
@@ -29,14 +30,40 @@
 
       var pinBuilder = new Cesium.PinBuilder();
 
-      var hospitalPin = Cesium.when(pinBuilder.fromMakiIconId('hospital', Cesium.Color.RED, 48), function(canvas) {
-    return viewer.entities.add({
-        name : 'Hospital',
-        position : Cesium.Cartesian3.fromDegrees(175.6223059,-40.3876416,15.45),
-        description:"memes for dreams<h1> big memes</h1>",
-        billboard : {
-            image : canvas.toDataURL(),
-            verticalOrigin : Cesium.VerticalOrigin.BOTTOM
-        }
-    });
-});
+    //   var hospitalPin = Cesium.when(pinBuilder.fromMakiIconId('hospital', Cesium.Color.RED, 48), function(canvas) {
+    // viewer.entities.add({
+    //     name : 'Hospital',
+    //     position : Cesium.Cartesian3.fromDegrees(175.6223059,-40.3876416,15.45),
+    //     description:"memes for dreams<h1> big memes</h1>",
+    //     billboard : {
+    //         image : canvas.toDataURL(),
+    //         verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+    //     }
+    // });
+// });
+
+
+
+$.get('./pins', {}, function(data){
+   pins=JSON.parse(data)
+
+
+   console.log(pins);
+
+   for(let i=0;i<pins.length;i++){
+      Cesium.when(pinBuilder.fromMakiIconId('hospital', Cesium.Color.RED, 48), function(canvas) {
+   let x =viewer.entities.add({
+         name : 'Hospital',
+         position : Cesium.Cartesian3.fromDegrees(pins[i]["lat"],pins[i]["lon"],15.45),
+         description:pins[i]["description"],
+         billboard : {
+             image : canvas.toDataURL(),
+             verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+         }
+     });
+     console.log(x);
+   })
+   }
+
+   console.log(data);
+ })
