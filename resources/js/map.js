bfa9d4bc7965010b360063c8044d1a2ca0a35c42
Lance Gray <lancegray1970@gmail.com>
2019-09-05 07:10:45 +1200

    Show pins modified to send map zoom window co-ordinates to backend

--- resources/js/map.js
+++ resources/js/map.js
@@ -2,65 +2,65 @@ Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 var viewer = new Cesium.Viewer('cesiumContainer');
 var pinBuilder = new Cesium.PinBuilder();
 var entities = [];
-var menuitems = [];
+var kmlmenu = [];
 var description = "";
 var image;
 var tag_types = ['airfield', 'airport', 'alcohol-shop', 'america-football', 'art-gallery', 'bakery', 'bank', 'bar', 'baseball', 'basketball', 'beer', 'bicycle', 'building', 'bus', 'cafe', 'camera', 'campsite', 'car', 'cemetery', 'cesium', 'chemist', 'cinema', 'circle', 'circle-stroked', 'city', 'clothing-store', 'college', 'commercial', 'cricket', 'cross', 'dam', 'danger', 'disability', 'dog-park', 'embassy', 'emergency-telephone', 'entrance', 'farm', 'fast-food', 'ferry', 'fire-station', 'fuel', 'garden', 'gift', 'golf', 'grocery', 'hairdresser', 'harbor', 'heart', 'heliport', 'hospital', 'ice-cream', 'industrial', 'land-use', 'laundry', 'library', 'lighthouse', 'lodging', 'logging', 'london-underground', 'marker', 'marker-stroked', 'minefield', 'mobilephone', 'monument', 'museum', 'music', 'oil-well', 'park2', 'parking-garage', 'parking', 'park', 'pharmacy', 'pitch', 'place-of-worship', 'playground', 'police', 'polling-place', 'post', 'prison', 'rail-above', 'rail-light', 'rail-metro', 'rail', 'rail-underground', 'religious-christian', 'religious-jewish', 'religious-muslim', 'restaurant', 'roadblock', 'rocket', 'school', 'scooter', 'shop', 'skiing', 'slaughterhouse', 'soccer', 'square', 'square-stroked', 'star', 'star-stroked', 'suitcase', 'swimming', 'telephone', 'tennis', 'theatre', 'toilets', 'town-hall', 'town', 'triangle', 'triangle-stroked', 'village', 'warehouse', 'waste-basket', 'water', 'wetland', 'zoo'];
+var toolbar = document.getElementById('toolbar');
+var scratchRectangle = new Cesium.Rectangle();
 viewer.infoBox.frame.removeAttribute('sandbox');
-// Add entities to Cesium object
-Sandcastle.addToolbarButton('Load Pins', function () {
-    $('#map-dialog').dialog('open');
-});
-Sandcastle.addToolbarButton('Show Pins', function () {
-    viewer.entities.removeAll();
-    var pins = JSON.parse(window.localStorage.getItem('pins'))
-    $.each(pins, function (key, value) {
-        description = "<p>Coordinates: (" + value.lon + ", " + value.lat + ")"
-                + "<hr>"
-                + value.description;
-        if (tag_types.includes(value.tag_type)) {
-            tag_type = pinBuilder.fromMakiIconId(value.tag_type, Cesium.Color.fromCssColorString(value.colour), 48);
-        } else {
-            tag_type = pinBuilder.fromColor(Cesium.Color.fromCssColorString(value.colour), 48);
-        }
-        
-        viewer.entities.add({
-            id: (value.uid).toString(),
-            name: value.name,
-            position: Cesium.Cartesian3.fromDegrees(value.lat, value.lon),
-            description: description,
-            point: {
-                show: false,
-                pixelSize: 4,
-                color: Cesium.Color.BLACK,
-                outlineColor: Cesium.Color.fromCssColorString(value.colour),
-                outlineWidth: 6
-            },
-            label: {
-                show: false,
-                text: (value.uid).toString(),
-                font: '16pt Arial',
-                fillColor: Cesium.Color.WHITE,
-                style: Cesium.LabelStyle.FILL,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                pixelOffset: new Cesium.Cartesian2(0, -12)
-            },
-            billboard: {
-                image: tag_type,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-            }
-        });
-    });
 
-    viewer.entities.add({
-        name: 'Red Box Demonstration',
-        position: Cesium.Cartesian3.fromDegrees(175.7, -40.4, 0),
-        box: {
-            dimensions: new Cesium.Cartesian3(30000.0, 30000.0, 0),
-            material: Cesium.Color.RED.withAlpha(0.1)
-        }
+// Add toolbar to Cesium map & load/hide pins
+Sandcastle.addToolbarButton('Show Pins', function () {
+    var rect = viewer.camera.computeViewRectangle(viewer.scene.globe.ellipsoid, scratchRectangle);
+    var area = "north=" + Cesium.Math.toDegrees(rect.north).toFixed(6) +
+            "&south=" + Cesium.Math.toDegrees(rect.south).toFixed(6) +
+            "&east=" + Cesium.Math.toDegrees(rect.east).toFixed(6) +
+            "&west=" + Cesium.Math.toDegrees(rect.west).toFixed(6) +
+            "&timeBegin=0" +
+            "&timeEnd=20000";
+    $.getJSON("getPins", area, function (data) {
+        viewer.entities.removeAll();
+        $.each(data, function (key, value) {
+            description = "<p>Coordinates: (" + value.lon + ", " + value.lat + ")"
+                    + "<hr>"
+                    + value.description;
+            if (tag_types.includes(value.tag_type)) {
+                tag_type = pinBuilder.fromMakiIconId(value.tag_type, Cesium.Color.fromCssColorString(value.colour), 48);
+            } else {
+                tag_type = pinBuilder.fromColor(Cesium.Color.fromCssColorString(value.colour), 48);
+            }
+
+            // Add entities
+            viewer.entities.add({
+                id: (value.uid).toString(),
+                name: value.name,
+                position: Cesium.Cartesian3.fromDegrees(value.lat, value.lon),
+                description: description,
+                point: {
+                    show: false,
+                    pixelSize: 4,
+                    color: Cesium.Color.BLACK,
+                    outlineColor: Cesium.Color.fromCssColorString(value.colour),
+                    outlineWidth: 6
+                },
+                label: {
+                    show: false,
+                    text: (value.uid).toString(),
+                    font: '16pt Arial',
+                    fillColor: Cesium.Color.WHITE,
+                    style: Cesium.LabelStyle.FILL,
+                    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                    pixelOffset: new Cesium.Cartesian2(0, -12)
+                },
+                billboard: {
+                    image: tag_type,
+                    verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+                }
+            });
+        });
+        viewer.zoomTo(viewer.entities);
     });
-    viewer.zoomTo(viewer.entities);
 });
 
 Sandcastle.addToolbarButton('Hide Pins', function () {
@@ -68,7 +68,7 @@ Sandcastle.addToolbarButton('Hide Pins', function () {
 });
 
 // Add KML data to Cesium object
-menuitems.push({
+kmlmenu.push({
     text: "Remove all KML Data",
     onselect: function () {
         viewer.dataSources.removeAll();
@@ -85,7 +85,7 @@ $.getJSON("getKML", function (data) {
             viewer.dataSources.add(kml);
             viewer.zoomTo(kml);
         };
-        menuitems.push(obj);
+        kmlmenu.push(obj);
     });
-    Sandcastle.addToolbarMenu(menuitems, 'toolbar');
+    Sandcastle.addToolbarMenu(kmlmenu, 'toolbar');
 });
