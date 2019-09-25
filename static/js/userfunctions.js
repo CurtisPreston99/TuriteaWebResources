1a8ff027f635577043dee2a352d824634bd93538
wizordgamming <wizordgamming@gmail.com>
2019-09-25 19:33:38 +1200

    updating user functions

--- static/js/userfunctions.js
+++ static/js/userfunctions.js
@@ -11,8 +11,35 @@ function adduser() {
 
   console.log(user);
 
-  $.post(url+"/api/addUser",pins,function(data){
+  $.post(url+"/api/addUser",user,function(data){
     console.log("posted");
     console.log(data);
   });
+}
+
+
+
+
+function updatePassword() {
+  passes={}
+  passes.old=calcMD5(document.getElementById("oldPassowrd").value);
+
+  passes.new=calcMD5(document.getElementById("newPassword").value);
+
+  console.log(passes);
+
+  $.post(url+"/api/changePassword",passes,function(data){
+    console.log("posted");
+    console.log(data);
+  });
+}
+
+function removeuser() {
+
+
+  $.post(url+"/api/changePassword",passes,function(data){
+    console.log("posted");
+    console.log(data);
+  });
+
 }
